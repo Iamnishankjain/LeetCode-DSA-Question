d2c07283f9d843de4e200bab8579270651d9b2de
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-05-03 23:59:32 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (49.33%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -2,26 +2,25 @@ class Solution {
     public int myAtoi(String s) {
         s=s.trim();
         if(s.length()==0) return 0;
-        int i=0, sign=1,num=0;
-        
-        if(s.charAt(i)=='-' || s.charAt(i)=='+'){
-            if(s.charAt(i)=='-'){
+        int i=0;
+        int sign=1;
+        if(s.charAt(0)=='-' || s.charAt(0)=='+'){
+            if(s.charAt(0)=='-'){
                 sign=-1;
             }
             i++;
         }
-        
-        while(i<s.length() && Character.isDigit(s.charAt(i))){
-            
-            int digit=s.charAt(i)-'0';
-            
-            if(num>(Integer.MAX_VALUE-digit)/10){
-                return sign==1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
-            
-            num=num*10+digit;
-            i++;
+        if (i >= s.length() || !Character.isDigit(s.charAt(i))) return 0;
+        return atoid(s,0,i,s.length(),sign);
+    }
+    public int atoid(String s,int num,int i,int n,int sign){
+        if(i==n || !Character.isDigit(s.charAt(i))){
+            return num*sign;
         }
-        return num*sign;
+        int digit=s.charAt(i)-'0';
+        if (num > (Integer.MAX_VALUE - digit) / 10) {
+            return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+        return atoid(s,num*10+digit,i+1,n,sign);
     }
 }
