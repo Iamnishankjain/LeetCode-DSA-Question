56758fc6054afa9b49e8804f8b83cf15e464266f
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-06-07 21:40:36 +0530

    Time: 3 ms (73.29%) | Memory: 46.1 MB (31.70%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
@@ -1,22 +1,19 @@
 class Solution {
     public List<String> validStrings(int n) {
         List<String> list=new ArrayList<>();
-        valid(list,n,n,"",n,-1);
+        valid(list,"",n,-1);
         return list;
     }
 
-    public void valid(List<String> list,int one,int zero,String s,int n,int flag){
-        if(s.length()>n){
-            return;
-        }
+    public void valid(List<String> list,String s,int n,int flag){
         if(s.length()==n){
             list.add(s);
             s="";
             return;
         }
-        if(zero>0 && flag!=0)
-        valid(list,one,zero-1,s+'0',n,0);
-        if(one>0)
-        valid(list,one-1,zero,s+'1',n,1);
+        if(flag!=0)
+        valid(list,s+'0',n,0);
+        
+        valid(list,s+'1',n,1);
     }
 }
