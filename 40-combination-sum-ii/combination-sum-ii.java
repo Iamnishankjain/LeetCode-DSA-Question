70d7f09eb453ee138cd37631a3db9920e9c95f02
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-06-10 21:56:04 +0530

    Time: 2 ms (99.74%) | Memory: 43.3 MB (53.52%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> ans=new ArrayList<>();
+        getSubset(0,target,ans,candidates,candidates.length,res);
+        return res;
+    }
+
+    public void getSubset(int ind,int target,List<Integer> ans,int[] arr,int n,List<List<Integer>> res){
+        if(target==0){
+            res.add(new ArrayList<>(ans));
+            return;
+        }
+        if(ind==n || target<0){
+            return;
+        }
+        for(int i=ind;i<n;i++){
+            if(i>ind && arr[i]==arr[i-1]) continue;
+            if(arr[i]>target) break;
+            ans.add(arr[i]);
+            getSubset(i+1,target-arr[i],ans,arr,n,res);
+            ans.remove(ans.size()-1);
+        }
+    }
+}
