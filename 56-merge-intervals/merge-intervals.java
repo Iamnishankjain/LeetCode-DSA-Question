4e0dce287e021c025d6a57ade4b7f21f0f485ff3
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-07-20 21:40:57 +0530

    Time: 9 ms (45.44%) | Memory: 46.6 MB (55.41%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -7,19 +7,12 @@ class Solution {
         for(int i=0;i<intervals.length;i++){
             int arr[]=list.get(list.size()-1);
             if(arr[1]>=intervals[i][0]){
-                arr[0]=Math.min(arr[0],intervals[i][0]);
                 arr[1]=Math.max(arr[1],intervals[i][1]);
-                list.set(list.size()-1,arr);
             }
             else{
                 list.add(intervals[i]);
             }
         }
-        int arr[][]=new int[list.size()][2];
-        for(int i=0;i<list.size();i++){
-            arr[i][0]=list.get(i)[0];
-            arr[i][1]=list.get(i)[1];
-        }
-        return arr;
+        return list.toArray(new int[list.size()][]);
     }
 }
