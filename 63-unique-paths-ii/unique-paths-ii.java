3342d6a2c8747460f13a55a720826cfc88fe9d2d
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-08-25 00:46:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (11.00%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int dp[]=new int[n];
+        for(int i=0;i<m;i++){
+            int temp[]=new int[n];
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1) temp[j]=0;
+                else if(i==0 && j==0) temp[j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up=dp[j];
+                    if(j>0) left=temp[j-1];
+                    temp[j]=up+left;
+                }
+            }
+            dp=temp;
+        }
+        return dp[n-1];
+    }
+}
