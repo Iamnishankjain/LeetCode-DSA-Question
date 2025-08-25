c114a89670f1648b46b294e6d4b0cfc1ad45bef1
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-08-26 00:10:30 +0530

    Time: 31 ms (28.74%) | Memory: 45 MB (93.23%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -2,18 +2,19 @@ class Solution {
     public int cherryPickup(int[][] grid) {
         int r=grid.length;
         int c=grid[0].length;
-		int dp[][][]=new int[r][c][c];
+		int dp[][]=new int[c][c];
 		for(int j1=0;j1<c;j1++){
             for(int j2=0;j2<c;j2++){
                 if(j1==j2){
-                    dp[r-1][j1][j2]=grid[r-1][j1];
+                    dp[j1][j2]=grid[r-1][j1];
                 }else{
-                    dp[r-1][j1][j2]=grid[r-1][j1]+grid[r-1][j2];
+                    dp[j1][j2]=grid[r-1][j1]+grid[r-1][j2];
                 }
             }
         }
 
         for(int i=r-2;i>=0;i--){
+            int temp[][]=new int[c][c];
             for(int j1=0;j1<c;j1++){
                 for(int j2=0;j2<c;j2++){
                     int max=Integer.MIN_VALUE;
@@ -24,17 +25,18 @@ class Solution {
                                 val+=grid[i][j2];
                             }
                             if(j1+k>=0 && j1+k<c && j2+l>=0 && j2+l<c)
-                                val+=dp[i+1][j1+k][j2+l];
+                                val+=dp[j1+k][j2+l];
                             else{
                                 val-=1e8;
                             }
                             max=Math.max(max,val);
                         }
                     }
-                    dp[i][j1][j2]= max;
+                    temp[j1][j2]= max;
                 }
             }
+            dp=temp;
         }
-		return dp[0][0][c-1];
+		return dp[0][c-1];
 	}
 }
