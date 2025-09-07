d6e049e414ccec979d5d4e0549e1ba3cc77fdf37
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-09-08 00:23:23 +0530

    Time: 18 ms (78.93%) | Memory: 51.2 MB (65.72%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -2,21 +2,20 @@ class Solution {
     public int numDistinct(String s1, String s2) {
         int n=s1.length();
         int m=s2.length();
-        int memo[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            Arrays.fill(memo[i],-1);
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
         }
-        return get(n-1,m-1,s1,s2,memo);
-    }
-    public static int get(int n,int m,String s1, String s2,int memo[][]){
-        if(m<0) return 1;
-        if(n<0) return 0;
-        if(memo[n][m]!=-1) return memo[n][m];
-        if(s1.charAt(n)==s2.charAt(m)){
-            return memo[n][m]=get(n-1,m-1,s1,s2,memo)+get(n-1,m,s1,s2,memo);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
         }
-        else{
-            return memo[n][m]=get(n-1,m,s1,s2,memo);
-        }
+        return dp[n][m];
     }
 }
