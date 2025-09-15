71b22e46c7c71325a588da73438e1fdf8c37ef26
Nishank Jain <158562827+Iamnishankjain@users.noreply.github.com>
2025-09-16 02:37:24 +0530

    Time: 5 ms (60.70%) | Memory: 45.1 MB (46.14%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,28 +2,26 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n=word1.length();
         int m=word2.length();
-        int memo[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            Arrays.fill(memo[i],-1);
+        int dp[][]=new int[n+1][m+1];
+        dp[0][0]=0;
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
         }
-        return min(n-1,m-1,word1,word2,memo);
-    }
-
-    public int min(int i,int j,String word1, String word2,int memo[][]){
-        if(i<0 && j<0) return 0;
-        if(i<0){
-            return j+1;
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
         }
-        if(j<0){
-            return i+1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    int insert=1+dp[i][j-1];
+                    int replace=1+dp[i-1][j-1];
+                    int delete=1+dp[i-1][j];
+                    dp[i][j]=Math.min(insert,Math.min(replace,delete));
+                }
+            }
         }
-        if(memo[i][j]!=-1) return memo[i][j];
-        if(word1.charAt(i)==word2.charAt(j)){
-            return min(i-1,j-1,word1,word2,memo);
-        }
-        int insert=1+min(i,j-1,word1,word2,memo);
-        int replace=1+min(i-1,j-1,word1,word2,memo);
-        int delete=1+min(i-1,j,word1,word2,memo);
-        return memo[i][j]=Math.min(insert,Math.min(replace,delete));
+        return dp[n][m];
     }
 }
